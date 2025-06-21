d542e2b74bb0093720fadedcb7a862ef440effcc
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-06-21 17:47:25 -0300

    criação do getGameById e entidade GameDTO

--- src/main/java/com/example/dslist/controllers/GameControllers.java
+++ src/main/java/com/example/dslist/controllers/GameControllers.java
@@ -2,9 +2,11 @@ package com.example.dslist.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.services.GameService;
 
@@ -18,4 +20,10 @@ public class GameControllers {
     public List<GameMinDTO > findAll(){
         return gameService.findAll();
     }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
+    }
+
 }
